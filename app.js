cab5ea608c388789011ee7207f53506cdd31b351
hsinyi8997 <hsinyi8997@gmail.com>
2021-11-06 15:13:49 +0800

    refactor: modify mongoose congif

--- app.js
+++ app.js
@@ -1,47 +1,19 @@
 //packages and setting
 const express = require('express')
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const routes = require('./routes')
+require('./config/mongoose')
 const methodOverride = require('method-override')
-const restaurantList = require('./models/restaurant')
-const restaurant = require('./models/restaurant')
-const app = express()
 const port = 3000
 
-//mongodb connection
-mongoose.connect('mongodb://localhost/restaurant-list')
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
-
-//handlebars and view engine
+const app = express()
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 app.use(methodOverride('_method'))
-
-//bodyParser
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(routes)
-
-//routing section
-
-
-
-
-
-
-
-
-
 
 //start express server
 app.listen(port, () => {
--- models/seeds/restaurantSeeders.js
+++ models/seeds/restaurantSeeders.js
@@ -1,16 +1,9 @@
-const mongoose = require('mongoose')
+const db = require('../../config/mongoose')
 const Restaurant = require('../restaurant')
 const restaurantData = require('../.././restaurant.json')
 const restaurants = restaurantData.results
-mongoose.connect('mongodb://localhost/restaurant-list')
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
 
 db.once('open', () => {
-  console.log('mongodb connected!')
   Restaurant.insertMany(restaurants)
   console.log('done')
 })
