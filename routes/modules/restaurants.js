5e38cedf1635163137cc4cce2a167f9f5e50a452
hsinyi8997 <hsinyi8997@gmail.com>
2021-11-08 15:06:46 +0800

    feat: add regExp to verify User's input value

--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -1,51 +1,64 @@
 const express = require('express')
 const router = express.Router()
 const restaurantList = require('../../models/restaurant')
+const regExpID = new RegExp(/[\w\S]{24,}/, 'i')
 
 router.get('/:restaurant_id', (req, res) => {
   const id = req.params.restaurant_id
-  return restaurantList.findById(id)
-    .lean()
-    .then(restaurant => res.render('show', { restaurant }))
-    .catch(error => console.log(error))
+  if (regExpID.test(id)) {
+    return restaurantList.findById(id)
+      .lean()
+      .then(restaurant => res.render('show', { restaurant }))
+      .catch(error => console.log(error))
+  } else res.send(data)
 })
 
 router.put('/', (req, res) => {
   const { name, name_en, category, image, location, phone, google_map, description } = req.body
   const rating = Number(req.body.rating)
-  return restaurantList.create({
-    name,
-    name_en,
-    category,
-    image,
-    location,
-    phone,
-    google_map,
-    rating,
-    description
-  })
-    .then(() => { res.redirect('/') })
-    .catch(error => console.log(error))
+  if (isNaN(rating)) {
+    res.send('Please go back to previous page and type number from 0-5 within rating box to create new restaurant.')
+  } else {
+    return restaurantList.create({
+      name,
+      name_en,
+      category,
+      image,
+      location,
+      phone,
+      google_map,
+      rating,
+      description
+    })
+      .then(() => { res.redirect('/') })
+      .catch(error => console.log(error))
+  }
 })
 
 router.delete('/:id', (req, res) => {
   const id = req.params.id
-  return restaurantList.findById(id)
-    .then(restaurant => restaurant.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
+  if (regExpID.test(id)) {
+    return restaurantList.findById(id)
+      .then(restaurant => restaurant.remove())
+      .then(() => res.redirect('/'))
+      .catch(error => console.log(error))
+  } else res.send(data)
+
 })
 
 router.get('/:id/edit', (req, res) => {
   const id = req.params.id
+  if (regExpID.test(id)) {
   return restaurantList.findById(id)
     .lean()
     .then(restaurant => res.render('edit', { restaurant }))
     .catch(error => console.log(error))
+  } else res.send(data)
 })
 
 router.put('/:id', (req, res) => {
   const id = req.params.id
+  if (regExpID.test(id)) {
   const { name, name_en, category, image, location, phone, google_map, rating, description } = req.body
   return restaurantList.findById(id)
     .then(restaurant => {
@@ -62,6 +75,7 @@ router.put('/:id', (req, res) => {
     })
     .then(() => res.redirect(`/restaurants/${id}`))
     .catch(error => console.log(error))
+  } else res.send(data)
 })
 
 module.exports = router
